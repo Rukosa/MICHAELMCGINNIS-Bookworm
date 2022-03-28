d59f201b86101c3ba0bef6045fa438df5689f2f6
Rukosa <mwmcginnis490@gmail.com>
2022-03-28 14:48:06 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-Bookworm/MICHAELMCGINNIS_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_BookwormApp.swift
+//  MICHAELMCGINNIS-Bookworm
+//
+//  Created by Michael Mcginnis on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
